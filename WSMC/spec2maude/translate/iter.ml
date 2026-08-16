64f9352fc621a14e63c785130259822277ba33c1
minsung-phy <minsung.phy@gmail.com>
2026-08-16 18:43:50 +0900

    dev: translator of iter

---
+++ WSMC/spec2maude/translate/iter.ml
@@ -0,0 +1,28 @@
+type iter =
+  | Opt                          (* `?` *)
+  | List                         (* `*` *)
+  | List1                        (* `+` *)
+  | ListN of exp * id option     (* `^` exp *)
+
+
+open Il.Ast
+open Maude_il
+
+
+(* SpecTec iteration -> Maude conditions *)
+
+let length value = App ("len", [value])
+
+let typecheck value element_type = BoolCond (App ("typecheck", [value; element_type]))
+
+let translate_conditions translate_count value element_type iter =
+  let check = typecheck value element_type in
+  match iter with
+  | Opt ->
+      [check; BoolCond (App ("_<=_", [length value; Const "1"]))]
+  | List ->
+      [check]
+  | List1 ->
+      [check; BoolCond (App ("_<_", [Const "0"; length value]))]
+  | ListN (count, _) ->
+      [check; EqCond (length value, translate_count count)]
